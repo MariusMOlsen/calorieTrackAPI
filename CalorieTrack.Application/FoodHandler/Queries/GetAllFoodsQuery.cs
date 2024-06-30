baf8f96ef94e2679efa71922413a63ecc12a6312
marius <mariusmoeolsen@gmail.com>
2024-06-30 15:04:26 +0200

    Added GetAllFoodQueryHandler, want to refactor some tables so I will go back to this functionality later

---
+++ CalorieTrack.Application/FoodHandler/Queries/GetAllFoodsQuery.cs
@@ -0,0 +1,10 @@
+﻿using CalorieTrack.Application.DTO;
+using ErrorOr;
+using MediatR;
+
+namespace CalorieTrack.Application.FoodHandler.Queries;
+
+public record GetAllFoodsQuery
+
+    (Guid userGuid) :  IRequest<ErrorOr<List<FoodDto>>>;
+
